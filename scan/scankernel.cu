6784325674eb839c20ffd47f0231ea16ef65c8a1
arnav dhamija <arnav.dhamija@gmail.com>
2025-05-29 21:23:31 -0700

    scan kernel

--- scan/scankernel.cu
+++ scan/scankernel.cu
@@ -39,6 +39,58 @@ __global__ void myBlockScan(float *data, int n) {
     }
 }
 
+__global__ void myShuffleScan(float *data, int n) {
+    __shared__ float sharedData[1024/PARTITION_SIZE];
+    int idx = threadIdx.x;
+    float threadValue = 0.f;
+    if (idx < n)
+    {
+        threadValue = data[idx];
+    }
+
+    auto tile = cg::tiled_partition<PARTITION_SIZE>(this_thread_block());
+
+    #pragma unroll
+    for (int i = 1; i < PARTITION_SIZE; i *= 2) {
+        // only template version has it wtf
+        float temp = tile.shfl_up(threadValue, i);
+        if (idx % PARTITION_SIZE >= i)
+        {
+            threadValue += temp;
+        }
+    }
+    if (tile.thread_rank()+1 == PARTITION_SIZE)
+    {
+        sharedData[idx/PARTITION_SIZE] = threadValue;
+    }
+    __syncthreads();
+
+    // for (int i = 1; i < PARTITION_SIZE; i *= 2) {
+    //     // only template version has it wtf
+    //     float temp = tile.shfl_up(threadValue, i);
+    //     if (idx % PARTITION_SIZE >= i)
+    //     {
+    //         threadValue += temp;
+    //     }
+    // }
+    if (threadIdx.x == 0)
+    {
+        for (int i = 1; i < 1024/PARTITION_SIZE; i++)
+        {
+            sharedData[i] += sharedData[i-1];
+        }
+    }
+    __syncthreads();
+
+    if (idx > PARTITION_SIZE)
+        threadValue += sharedData[idx/PARTITION_SIZE-1];
+
+    if (idx < n)
+    {
+        data[idx] = threadValue;
+    }
+}
+
 
 int main(int argc, char **argv)
 {
@@ -59,7 +111,8 @@ int main(int argc, char **argv)
     }
 
     cudaMemcpy(gpuData, data, n*sizeof(float), cudaMemcpyHostToDevice);
-    myBlockScan<<<1,1024>>>(gpuData,n);
+    // myBlockScan<<<1,1024>>>(gpuData,n);
+    myShuffleScan<<<1,1024>>>(gpuData,n);
     cudaMemcpy(gpuResultCpu, gpuData, n*sizeof(float), cudaMemcpyDeviceToHost);
     for (int i = 1; i < n; i++)
     {
